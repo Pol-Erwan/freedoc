08cd889776c902c6a4d39d5f58e8acff3adb9b34
Pol-Erwan <polerwankite@gmail.com>
2019-10-24 15:44:33 +0200

    15h45

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
-# README
+Premier projet, la BDD FreeDoc
+création d'une BDD pour les docteurs
+création d'une BDD pour les patients
+création d'une BDD pour les rdv
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
---
+++ db/seed.rb
@@ -0,0 +1,25 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+
+doctor_array = []
+patient_array= []
+
+6.times do |index|
+ doctor_array << Doctor.create(id:index, first_name: Faker::Name.first_name, last_name: Faker::Name.middle_name, speciality:["Docteur de la tête","Docteur des pieds","Docteur du ventre","Docteur du dos"].sample, zip_code: Faker::Address.zip_code)
+ patient_array << Patient.create(id:index, first_name: Faker::Name.first_name, last_name: Faker::Name.middle_name)
+end
+
+6.times do |patient|
+ Appointment.create(date: Faker::Time.forward(days: 28),doctor: doctor_array.sample, patient: patient_array.sample)
+end
